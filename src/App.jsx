1fab54f47c3229db9fdd68103528db349b77632b
bokyoungp <bokyoungp@gmail.com>
2026-04-28 15:45:22 +0900

    routing - react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,17 @@
-import { useState, useEffect } from "react";
-import Movie from "./Movie";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 import "./App.css";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [datas, setDatas] = useState([]);
-
-  const getMovies = async () => {
-    const response = await fetch("http://localhost:3000/movies");
-    const json = await response.json();
-    setDatas(json);
-    setIsLoading(false);
-  };
-
-  // 서버로부터 데이터 딱 한번만 가져오기
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
-    <>
-      <h1>Movie json-server API</h1>
-      <hr />
-      {isLoading ? <p>IsLoading...</p> : null}
-      <div>
-        {datas.map((data) => (
-          <div key={data.id}>
-            <Movie
-              poster={data.poster}
-              genres={data.genres}
-              title={data.title}
-            />
-          </div>
-        ))}
-      </div>
-      <ul></ul>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/routes/Detail.jsx
@@ -0,0 +1,9 @@
+function Detail() {
+  return (
+    <div>
+      <h1>Movie detail</h1>
+    </div>
+  );
+}
+
+export default Detail;
